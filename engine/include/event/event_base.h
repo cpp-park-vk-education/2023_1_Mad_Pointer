6b5801542f99641f3732f54d4fb1c1e5d3fff66f
dima <dima>
2023-05-09 07:20:35 +0300

    add event base class

---
+++ engine/include/event/event_base.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+namespace ecs::event {
+    using EventTypeId = size_t;
+
+    class EventBase {
+    public:
+        EventBase(EventTypeId typeId);
+        inline const EventTypeId	getEventTypeID()	const { return this->m_TypeId; }
+        inline const EventTimestamp getTimeCreated()	const { return this->m_TimeCreated; }
+    private:
+        EventTypeId		m_TypeId;
+        EventTimestamp	m_TimeCreated;
+    };
+}
