fe4b8f0dd14d725b1f190e0b26da3eeb3116d789
dima <dima>
2023-05-09 05:00:51 +0300

    add Entity Manager class

--- engine/include/ecs/entity_manager/entity_manager.h
+++ engine/include/ecs/entity_manager/entity_manager.h
@@ -1,8 +1,86 @@
-//
-// Created by dmitry on 04.05.2023.
-//
+#include <string>
+#include "../entitty/entity_base.h"
+#include <typeinfo>
+#include <unordered_map>
+#include <vector>
 
-#ifndef INC_2023_1_MAD_POINTER_ENTITY_MANAGER_H
-#define INC_2023_1_MAD_POINTER_ENTITY_MANAGER_H
+namespace ecs {
+    class EntityManager {
+        class EntityContainerBase {
+        public:
+            virtual ~EntityContainerBase() {}
 
-#endif //INC_2023_1_MAD_POINTER_ENTITY_MANAGER_H
+            virtual std::string getEntityContainerTypeName() const = 0;
+
+            virtual void DestroyEntity(EntityBase* object) = 0;
+
+        };
+
+        template<typename EntityClassType>
+        class EntityContainer : public EntityContainerBase {
+        public:
+            EntityContainer(const EntityContainer&) = delete;
+            EntityContainer &operator=(EntityContainer &) = delete;
+
+            EntityContainer() {}
+
+            virtual ~EntityContainer() {}
+
+            virtual std::string getEntityContainerTypeName() const override {
+                static std::string ENTITY_TYPE_NAME{typeid(EntityClassType).name()};
+                return ENTITY_TYPE_NAME;
+            }
+
+            virtual void DestroyEntity(EntityBase* object) override {
+                object->~EntityBase();
+                this->DestroyObject(object);
+            }
+        private:
+            // place container here
+        };
+
+        using EntityTypeId = std::size_t;
+        using EntityId = std::size_t;
+        using EntityRegistry = std::unordered_map<EntityTypeId, EntityContainerBase*>;
+        using PendingDestroyedEntities = std::vector<EntityId>;
+        using EntityHandleTable = ecs::utils::HandleTable<EntityContainerBase, EntityId>;
+
+    public:
+        EntityManager(const EntityManager&) = delete;
+        EntityManager& operator=(EntityManager&) = delete;
+
+        template<class EntityClassType>
+        inline EntityContainer<EntityClassType>* getEntityContainer() { }
+
+        EntityId acquireEntityId(EntityBase* entity);
+
+        void ReleaseEntityId(EntityId id);
+
+        EntityManager(ComponentManager* componentManagerInstance);
+        ~EntityManager();
+
+        template<class EntityClassType, class... Args>
+        EntityId CreateEntity(Args&&... args) {}
+
+        void DestroyEntity(EntityId entityId) {}
+
+        inline EntityBase* getEntity(EntityId entityId) {
+            return m_entityHandleTable[entityId];
+        }
+
+        inline EntityId getEntityId(EntityId::valueType index) const {
+            return m_entityHandleTable[index];
+        }
+
+        void RemoveDestroyedEntities();
+
+    private:
+
+
+        EntityRegistry m_entityRegistry;
+        PendingDestroyedEntities m_pendingDestroyedEntities;
+        size_t m_numPendingDestroyedEntities;
+        ComponentManager*    m_componentManagerInstance;
+
+    };
+}
