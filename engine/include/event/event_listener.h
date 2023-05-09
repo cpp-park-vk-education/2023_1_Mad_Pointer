9e01bac5c1912231fd247c8bceea85be9c216e78
dima <dima>
2023-05-09 07:59:47 +0300

    add event listener

---
+++ engine/include/event/event_listener.h
@@ -0,0 +1,23 @@
+#include "event_delegate.h"
+#include <list>
+namespace ecs::event {
+    class EventListenerBase {
+        using RegisteredCallbacks = std::list<internal::EventBaseDelegate*>;
+
+    public:
+
+        EventListenerBase();
+        virtual ~EventListenerBase();
+
+        template<class Event, class Callback>
+        inline void registerEventCallback(void(Callback::*callback)(const Event* const)) {}
+
+        template<class Event, class Callback>
+        inline void unregisterEventCallback(void(Callback::*callback)(const Event* const)) {}
+
+        void unregisterAllEventCallbacks();
+    private:
+        RegisteredCallbacks m_RegisteredCallbacks;
+    };
+}
+
