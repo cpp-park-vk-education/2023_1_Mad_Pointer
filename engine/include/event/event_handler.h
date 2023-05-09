19a8ba812d99504b17f25933bd0f535c16174e41
dima <dima>
2023-05-09 08:09:44 +0300

    add event handler class

---
+++ engine/include/event/event_handler.h
@@ -0,0 +1,41 @@
+#include "event_base.h"
+#include "event_dispatcher.h"
+#include <vector>
+#include <unordered_map>
+
+namespace ecs::event {
+
+    class  EventHandler {
+        friend class ECSEngine;
+        using EventDispatcherMap = std::unordered_map<EventTypeId, internal::EventBaseDispatcher*>;
+        using EventStorage = std::vector<EventBase*>;
+    public:
+        EventHandler();
+        ~EventHandler();
+
+        inline void ClearEventBuffer() {
+            m_EventStorage.clear();
+        }
+
+        inline void ClearEventDispatcher() {
+            m_EventDispatcherMap.clear();
+        }
+
+        template<class E, class... ARGS>
+        void Send(ARGS&&... eventArgs) {}
+
+        void DispatchEvents() {}
+
+    private:
+        EventHandler(const EventHandler&);
+        EventHandler& operator=(EventHandler&);
+
+        EventDispatcherMap	m_EventDispatcherMap;
+        EventStorage m_EventStorage;
+
+        template<class E>
+        inline void AddEventCallback(internal::EventBaseDelegate* const eventDelegate) { }
+
+        inline void RemoveEventCallback(internal::EventBaseDelegate* eventDelegate) { }
+    };
+}
