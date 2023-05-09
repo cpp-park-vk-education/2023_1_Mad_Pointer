dd0f323964eb7d6f6a383b592c2402d91b66bd25
dima <dima>
2023-05-09 05:17:46 +0300

    add Component Manager class

--- engine/include/ecs/component_manager/component_manager.h
+++ engine/include/ecs/component_manager/component_manager.h
@@ -1,2 +1,90 @@
 #include "../component/base_component.h"
+#include <string>
+#include <typeinfo>
+#include <unordered_map>
+#include <vector>
+namespace ecs {
+    class  ComponentManager  {
+        class ComponentContainerBase {
+        public:
+            virtual ~ComponentContainerBase() {}
+            virtual std::string getComponentContainerTypeName() const = 0;
+            virtual void destroyComponent(ComponentBase* object) = 0;
+        };
 
+        template<typename ComponentClassType>
+        class ComponentContainer : public ComponentContainerBase {
+            ComponentContainer(const ComponentContainer&) = delete;
+            ComponentContainer& operator=(ComponentContainer&) = delete;
+        public:
+            ComponentContainer() {}
+
+            virtual ~ComponentContainer() {}
+
+            virtual std::string getComponentContainerTypeName() const override {
+                static std::string COMPONENT_TYPE_NAME{ typeid(ComponentClassType).name() };
+                return COMPONENT_TYPE_NAME;
+            }
+
+            virtual void destroyComponent(ComponentBase* object) override {
+                object->~ComponentBase();
+                this->destroyObject(object);
+            }
+
+        };
+
+    public:
+        ComponentManager(const ComponentManager&) = delete;
+        ComponentManager& operator=(ComponentManager&) = delete;
+
+        using ComponentTypeId = size_t;
+        using ComponentId = size_t;
+        using ComponentContainerRegistry = std::unordered_map<ComponentTypeId, ComponentContainerBase*>;
+        using ComponentLookupTable = std::vector<ComponentBase*>;
+        using EntityComponentMap = std::vector<std::vector<ComponentId>>;
+
+
+        template<typename ComponentClassType>
+        inline ComponentContainer<ComponentClassType>* getComponentContainer() {}
+
+        ComponentId	acquireComponentId(ComponentBase* component);
+        void releaseComponentId(ComponentId id);
+
+        void mapEntityComponent(EntityId entityId, ComponentId componentId, ComponentTypeId componentTypeId);
+        void unmapEntityComponent(EntityId entityId, ComponentId componentId, ComponentTypeId componentTypeId);
+
+        template<typename ComponentClassType>
+        using TemplateComponentIterator = typename ComponentContainer<ComponentClassType>::iterator;
+
+        ComponentManager();
+        ~ComponentManager();
+
+        template<class ComponentClassType, class ...Args>
+        ComponentClassType* addComponent(const EntityId entityId, Args&&... args) {}
+
+        template<class ComponentClassType>
+        void removeComponent(const EntityId entityId) {}
+
+
+        void removeAllComponents(const EntityId entityId) {}
+
+        template<class ComponentClassType>
+        ComponentClassType* getComponent(const EntityId entityId) {}
+
+        template<class ComponentClassType>
+        inline TemplateComponentIterator<ComponentClassType> begin() {
+            return GetComponentContainer<ComponentClassType>()->begin();
+        }
+
+        template<class ComponentClassType>
+        inline TemplateComponentIterator<ComponentClassType> end() {
+            return GetComponentContainer<ComponentClassType>()->end();
+        }
+
+    private:
+        EntityComponentMap m_EntityComponentMap;
+        ComponentContainerRegistry m_ComponentContainerRegistry;
+        ComponentLookupTable m_ComponentLUT;
+
+    };
+} // namespace ECS
--- engine/include/ecs/entitty/entity_base.h
+++ engine/include/ecs/entitty/entity_base.h
@@ -1,43 +1,48 @@
-class EntityBase {
-public:
-    EntityBase();
-    virtual ~EntityBase();
+#include "../component_manager/component_manager.h"
 
-    template<typename ComponentType>
-    ComponentType* getComponent() const {
-        return m_componentManagerInstance->getComponent<ComponentType>(m_entityId);
-    }
+namespace ecs {
+    class EntityBase {
+        using EntityTypeId = size_t;
+    public:
+        EntityBase();
+        virtual ~EntityBase();
 
-    template <typename ComponentType, typename ...Args>
-    ComponentType* addComponent(Args&&... args) {
-        return m_componentManagerInstance->addComponent<ComponentType>(m_enityId, args);
-    }
+        template<typename ComponentType>
+        ComponentType* getComponent() const {
+            return m_componentManagerInstance->getComponent<ComponentType>(m_entityId);
+        }
 
-    template<typename ComponentType>
-    ComponentType* removeComponent() const {
-        return m_componentManagerInstance->removeComponent<ComponentType>(m_entityId);
-    }
+        template <typename ComponentType, typename ...Args>
+        ComponentType* addComponent(Args&&... args) {
+            return m_componentManagerInstance->addComponent<ComponentType>(m_enityId, std::forward<>(args...));
+        }
 
-    virtual EntityTypeId getStaticEntityTypeId() const = 0;
+        template<typename ComponentType>
+        ComponentType* removeComponent() const {
+            return m_componentManagerInstance->removeComponent<ComponentType>(m_entityId);
+        }
 
-    inline EntityId getEntityId() const {
-        return m_entityId;
-    }
+        virtual EntityTypeId getStaticEntityTypeId() const = 0;
 
-    void setActive(bool active) {
-        m_isActive = active;
-    }
+        inline EntityId getEntityId() const {
+            return m_entityId;
+        }
 
-    inline bool isActive() const {
-        return m_isActive;
-    }
+        void setActive(bool active) {
+            m_isActive = active;
+        }
 
-    virtual void onEnable() {}
-    virtual void onDisable() {}
-protected:
-    EntityId m_entityId; // создаем при инициализации инстанса entity base'a;
-    bool m_isActive = true;
-private:
-    ComponentManager* m_componentManagerInstance;
+        inline bool isActive() const {
+            return m_isActive;
+        }
 
-};
+        virtual void onEnable() {}
+        virtual void onDisable() {}
+    protected:
+        EntityId m_entityId; // создаем при инициализации инстанса entity base'a;
+        bool m_isActive = true;
+    private:
+        ComponentManager* m_componentManagerInstance;
+
+    };
+}
