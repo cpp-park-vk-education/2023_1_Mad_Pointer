c0d5d90775cc940438c709fff9c75a764fe07f37
dima <dima>
2023-05-14 02:30:40 +0300

    add transform

---
+++ engine/game_components/transform_component.h
@@ -0,0 +1,17 @@
+#ifndef MY_PROJECT_TRANSFORM_COMPONENT_H
+#define MY_PROJECT_TRANSFORM_COMPONENT_H
+
+class TransformComponent : public ECS::Component<TransformComponent> {
+private:
+    Transform	m_Transform;
+public:
+    TransformComponent();
+    TransformComponent(const mat3& transform);
+    TransformComponent(const Transform& transform);
+    virtual ~TransformComponent();
+    inline void SetTransform(const glm::mat4& transform) {this->m_Transform = transform;}
+    inline Transform* AsTransform() { return &(this->m_Transform); }
+    inline const float*		AsFloat() const { return this->m_Transform; }
+    inline const mat3& AsMat3() const { return this->m_Transform; }
+};
+#endif //MY_PROJECT_TRANSFORM_COMPONENT_H
