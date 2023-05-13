0109aaa323e9742c7ec4091858888dfcdb969cd9
dima <dima>
2023-05-14 00:58:37 +0300

    add controller system

--- engine/game_systems/controller_system.h
+++ engine/game_systems/controller_system.h
@@ -1,8 +1,22 @@
-//
-// Created by dmitry on 13.05.23.
-//
-
 #ifndef MY_PROJECT_CONTROLLER_SYSTEM_H
 #define MY_PROJECT_CONTROLLER_SYSTEM_H
+
+class ControllerSystem :  public ECS::Event::IEventListener {
+    using RegisteredController = std::list<Controller*>;
+public:
+
+    ControllerSystem();
+    virtual ~ControllerSystem();
+
+    void update(float deltaTime);
+
+    void RegisterController(Controller* controller);
+    void UnregisterController(Controller* controller);
+private:
+    void RegisterEventCallbacks();
+    void UnregisterEventCallbacks();
+    RegisteredController m_Controller;
+};
+
 
 #endif //MY_PROJECT_CONTROLLER_SYSTEM_H
