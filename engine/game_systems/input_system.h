02c0ca5d6300728f1dab01476acb0d3ada4edabc
dima <dima>
2023-05-14 01:00:46 +0300

    add input system

--- engine/game_systems/input_system.h
+++ engine/game_systems/input_system.h
@@ -1,8 +1,17 @@
-//
-// Created by dmitry on 13.05.23.
-//
-
 #ifndef MY_PROJECT_INPUT_SYSTEM_H
 #define MY_PROJECT_INPUT_SYSTEM_H
+class InputSystem {
+private:
 
+    int				m_NumKeys;
+    unsigned char*	m_OldKeyStates;
+
+public:
+
+    InputSystem();
+    ~InputSystem();
+
+    void PreUpdate(float deltaTime) ;
+
+};
 #endif //MY_PROJECT_INPUT_SYSTEM_H
