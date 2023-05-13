1f1c4de8b9462ca42388a89ebc364264e1dda407
dima <dima>
2023-05-14 01:11:48 +0300

    add lifetime system

--- engine/game_systems/lifetime_sytem.h
+++ engine/game_systems/lifetime_sytem.h
@@ -1,8 +1,32 @@
-//
-// Created by dmitry on 13.05.23.
-//
-
 #ifndef MY_PROJECT_LIFETIME_SYTEM_H
 #define MY_PROJECT_LIFETIME_SYTEM_H
+#include "../include/event/event_listener.h"
+
+class LifetimeSystem : public ecs::event::EventListenerBase {
+    using ActiveLifetimeComponents = std::list<LifetimeComponent*>;
+private:
+
+    void RegisterEventCallbacks();
+    void UnregisterEventCallbacks();
+
+    void OnGameObjectCreated(const GameObjectCreated* event);
+    void OnGameObjectDestroyed(const GameObjectDestroyed* event);
+    void OnGameObjectSpawned(const GameObjectSpawned* event);
+    void OnGameObjectKilled(const GameObjectKilled* event);
+
+    WorldSystem* m_WorldSystem;
+
+    ActiveLifetimeComponents m_ActiveLifetimeComponents;
+
+public:
+
+    LifetimeSystem();
+    virtual ~LifetimeSystem();
+
+    void Update(float deltaTime);
+
+    void Reset();
+
+};
 
 #endif //MY_PROJECT_LIFETIME_SYTEM_H
