f4415583d08f173b82a6c601e9027941de3a1f80
dima <dima>
2023-05-14 06:37:41 +0300

    add orthogonal camera and table top camera

---
+++ engine/camera/orthogonal_camera.h
@@ -0,0 +1,23 @@
+#ifndef MY_PROJECT_ORTHOGONAL_CAMERA_H
+#define MY_PROJECT_ORTHOGONAL_CAMERA_H
+#include "camera.h"
+class OrthogonalCamera : public Camera {
+private:
+
+    float m_Zoom;
+
+public:
+
+    OrthogonalCamera(const glm::vec3& position, const float zoom = 1.0f);
+    virtual ~OrthogonalCamera();
+
+    virtual Transform GetViewTransform() const override;
+    virtual Transform GetProjectionTransform() const override;
+
+    void TurnLeft(const float degrees);
+    void TurnRight(const float degrees);
+
+    inline void SetZoom(const float zoom) {}
+
+};
+#endif //MY_PROJECT_ORTHOGONAL_CAMERA_H
