f49b88a7057469dd630139267eb9b9e4d488d550
dima <dima>
2023-05-19 11:31:41 +0300

    add event dispatcher

--- engine/include/event/event_dispatcher.h
+++ engine/include/event/event_dispatcher.h
@@ -37,11 +37,44 @@ namespace ecs::event::internal {
         }
 
         virtual void addEventCallback(EventBaseDelegate* const eventDelegate) override {
+            auto result = std::find_if(m_PendingRemoveDelegates.begin(), m_PendingRemoveDelegates.end(),
+                                       [&](const EventBaseDelegate* other) {
+                                           return *eventDelegate == other;
+                                       });
 
+            if (result != m_PendingRemoveDelegates.end()) {
+                m_PendingRemoveDelegates.erase(result);
+                return;
+            }
+
+            m_EventCallbacks.push_back(eventDelegate);
         }
 
         virtual void removeEventCallback(EventBaseDelegate* eventDelegate) override {
+            if (!m_Locked) {
+                auto result = std::find_if(m_EventCallbacks.begin(), m_EventCallbacks.end(),
+                                           [&](const EventBaseDelegate* other) {
+                                               return *other == eventDelegate;
+                                           });
 
+                if (result != m_EventCallbacks.end()) {
+                    EventBaseDelegate* ptrMem = (EventBaseDelegate*)(*result);
+
+                    m_EventCallbacks.erase(result);
+
+                    delete ptrMem;
+                    ptrMem = nullptr;
+                }
+            }
+            else {
+                auto result = std::find_if(m_EventCallbacks.begin(), m_EventCallbacks.end(),
+                                           [&](const EventBaseDelegate *other) {
+                                               return other == eventDelegate;
+                                           });
+
+                assert(result != m_EventCallbacks.end() && "");
+                m_PendingRemoveDelegates.push_back((*result));
+            }
         }
 
         virtual inline size_t getEventCallbackCount() const override { return this->m_EventCallbacks.size(); }
