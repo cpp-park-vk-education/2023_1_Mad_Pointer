c9486cf7bf92c0d780e0d736da5b6508381a8156
dima <dima>
2023-05-19 11:34:34 +0300

    impl event base

--- engine/include/event/event_base.h
+++ engine/include/event/event_base.h
@@ -1,15 +1,14 @@
 #pragma once
+#include "../../utils/timer.h"
 #include <iostream>
 
 namespace ecs::event {
     using EventTypeId = size_t;
-    struct EventTimestamp {
-
-    };
+    using EventTimestamp = utils::TimeStamp;
 
     class EventBase {
     public:
-        EventBase(EventTypeId typeId);
+        EventBase(EventTypeId typeId) : m_TimeCreated(utils::Timer().GetTimeStamp()), m_TypeId(typeId) {}
         inline const EventTypeId	getEventTypeID()	const { return this->m_TypeId; }
         inline const EventTimestamp getTimeCreated()	const { return this->m_TimeCreated; }
     private:
