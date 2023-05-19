855f24ffb2cfe98e12a74aa9bc9f52f7e3cebd15
dima <dima>
2023-05-19 12:08:46 +0300

    impl event listener

--- engine/include/event/event_listener.h
+++ engine/include/event/event_listener.h
@@ -1,5 +1,7 @@
 #pragma once
+#include "api.h"
 #include "event_delegate.h"
+
 #include <list>
 namespace ecs::event {
     class EventListenerBase {
@@ -11,12 +13,33 @@ namespace ecs::event {
         virtual ~EventListenerBase() {}
 
         template<class Event, class Callback>
-        inline void registerEventCallback(void(Callback::*callback)(const Event* const)) {}
+        inline void registerEventCallback(void(Callback::*callback)(const Event* const)) {
+            internal::EventBaseDelegate* eventDelegate = new internal::EventDelegate<Callback, Event>(static_cast<Callback*>(this), callback);
+
+            m_RegisteredCallbacks.push_back(eventDelegate);
+            ECS_Engine->SubscribeEvent<Event>(eventDelegate);
+        }
 
         template<class Event, class Callback>
-        inline void unregisterEventCallback(void(Callback::*callback)(const Event* const)) {}
+        inline void unregisterEventCallback(void(Callback::*callback)(const Event* const)) {
+            internal::EventDelegate<Callback, Event> delegate(static_cast<Callback*>(this), callback);
 
-        void unregisterAllEventCallbacks() {}
+            for (auto cb : this->m_RegisteredCallbacks) {
+                if (cb->getDelegateId() == delegate.GetDelegateId()) {
+                    m_RegisteredCallbacks.remove_if(
+                            [&](const internal::EventBaseDelegate* other)
+                            {
+                                return other->operator==(cb);
+                            }
+                    );
+
+                    ECS_Engine->UnsubscribeEvent(&delegate);
+                    break;
+                }
+            }
+        }
+
+        void unregisterAllEventCallbacks();
     private:
         RegisteredCallbacks m_RegisteredCallbacks;
     };
