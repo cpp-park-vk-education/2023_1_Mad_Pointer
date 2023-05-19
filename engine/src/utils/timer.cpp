fd5ce87e5f24f77175c27ac6c51f8c4ec1b7e1e0
dima <dima>
2023-05-19 04:21:54 +0300

    define and impl timer

---
+++ engine/src/utils/timer.cpp
@@ -0,0 +1,15 @@
+#include "../../include/utils/timer.h"
+
+namespace ecs::utils {
+    Timer::Timer() : m_Elapsed(0) {}
+
+    Timer::~Timer() {}
+
+    void Timer::Tick(float ms) {
+        m_Elapsed += std::chrono::duration<float, std::ratio<1, 1000>>(ms);
+    }
+
+    void Timer::Reset() {
+        m_Elapsed = Elapsed::zero();
+    }
+}
