501a21ba7025f598fb82ff0df98eaf6c29b39899
kayetana <Ane4kaGr@mail.ru>
2023-05-25 13:05:03 +0300

    change sdl to sfml

--- engine/include/game_systems/input_system.h
+++ engine/include/game_systems/input_system.h
@@ -3,6 +3,8 @@
 #include "system.h"
 #include "event.h"
 
+#include <SFML/Graphics.hpp>
+
 class InputSystem : ecs::System<InputSystem> {
 public:
     InputSystem();
@@ -11,6 +13,31 @@ public:
     void preUpdate(float deltaTime) override;
 
 private:
-    int	m_NumKeys;
-    unsigned char* m_OldKeyStates;
+    sf::RenderWindow window;
 };
+
+void InputSystem::preUpdate(float deltaTime) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+        switch (event.type) {
+            case sf::Event::Closed:
+                // window.close();
+                break;
+            case sf::Event::KeyPressed:
+                // pressed
+                break;
+            case sf::Event::KeyReleased:
+                // released
+                break;
+            case sf::Event::MouseButtonPressed:
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    // mouse left
+                    // event.mouseButton.x
+                    // event.mouseButton.y
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
