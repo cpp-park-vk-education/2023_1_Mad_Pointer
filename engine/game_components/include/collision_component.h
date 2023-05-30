d311279eb25f9fef8c1e06f52fe8b6f1a8a72285
dima <dima>
2023-05-30 22:03:52 +0300

    add collisions

---
+++ engine/game_components/include/collision_component.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "component.h"
+#include <memory>
+#include <SFML/Graphics.hpp>
+
+enum CollisionType {
+    EnemyCollisionType,
+    PlayerCollisionType,
+    WallCollisionType
+};
+
+class CollisionComponent : public ecs::Component<CollisionComponent> {
+public:
+    CollisionComponent(const ecs::EntityId id, CollisionType collisionType) : m_collisionType(collisionType) {}
+    ~CollisionComponent() override = default;
+
+
+private:
+    CollisionType m_collisionType;
+};
