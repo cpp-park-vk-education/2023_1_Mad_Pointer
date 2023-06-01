fca5c175ca3253922edbbafee981b5b43e29ba2a
kayetana <Ane4kaGr@mail.ru>
2023-06-01 18:30:03 +0300

    remove logic from main

--- game/main.cpp
+++ game/main.cpp
@@ -2,95 +2,8 @@
 #include "logger.h"
 #include "menu/include/menu.h"
 
-using namespace sf;
-void GamеStart() {
-    RenderWindow Play(VideoMode::getDesktopMode(), L"Уровень 1", Style::Fullscreen);
-
-    RectangleShape background_play(Vector2f(1920, 1080));
-
-    Texture texture;
-    if (!texture.loadFromFile("image.jpg")) exit(1);
-    background_play.setTexture(&texture);
-
-    while (Play.isOpen()) {
-        Event event_play;
-        while (Play.pollEvent(event_play)) {
-            if (event_play.type == Event::KeyPressed)
-            {
-                if (event_play.key.code == Keyboard::Escape) { Play.close(); }
-            }
-        }
-        Play.clear();
-        Play.draw(background_play);
-        Play.display();
-    }
-}
-
-void InitText(Text& mtext,
-              float xpos,
-              float ypos,
-              String str,
-              int size_font = 60,
-              Color menu_text_color = Color::White,
-              int bord = 0,
-              Color border_color = Color::Black);
-
 int main() {
-    RenderWindow window;
-    window.create(VideoMode::getDesktopMode(), L"Моя игра", Style::Fullscreen);
-
-    window.setMouseCursorVisible(false);
-    float width = VideoMode::getDesktopMode().width;
-    float height = VideoMode::getDesktopMode().height;
-
-    RectangleShape background(Vector2f(width, height));
-    Texture texture_window;
-    if (!texture_window.loadFromFile("image/menu_background.jpg")) return 4;
-    background.setTexture(&texture_window);
-
-    Font font;
-    if (!font.loadFromFile("font/MyriadPro.otf")) return 5;
-    Text title;
-    title.setFont(font);
-
-    InitText(title, 480, 50, L"Maddest Pointer", 150, Color(237, 147, 0), 3);
-    String name_menu[]{L"Стрелялка", L"Машинки", L"Выход"};
-
-    GameMenu menu(window, 950, 350, 3, name_menu, 100, 120);
-    menu.setMenuTextColor(Color(237, 147, 0), Color::Red, Color::Black);
-    menu.alignMenuPosition(2);
-
-    while (window.isOpen()) {
-        Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == Event::KeyReleased) {
-                if (event.key.code == Keyboard::Up) {
-                    menu.moveUp();
-                }
-                if (event.key.code == Keyboard::Down) {
-                    menu.moveDown();
-                }
-                if (event.key.code == Keyboard::Return) {
-                    switch (menu.getNumOfSelectedItem()) {
-                        case 0:
-                            std::cout << "start first game" << std::endl;
-                            break;
-                        case 1:
-                            std::cout << "start second game" << std::endl;
-                            break;
-                        case 2:
-                            window.close();
-                            break;
-                    }
-                }
-            }
-        }
-        window.clear();
-        window.draw(background);
-        window.draw(title);
-        menu.draw();
-        window.display();
-    }
+    start();
 
     //GamеStart();
     // GameLoop gameLoop("The Floors");
@@ -100,13 +13,4 @@ int main() {
     // gameLoop.initializeECS();
     // gameLoop.run();
     return 0;
-}
-
-void InitText(Text& mtext, float xpos, float ypos, String str, int size_font, Color menu_text_color, int bord, Color border_color) {
-    mtext.setCharacterSize(size_font);
-    mtext.setPosition(xpos, ypos);
-    mtext.setString(str);
-    mtext.setFillColor(menu_text_color);
-    mtext.setOutlineThickness(bord);
-    mtext.setOutlineColor(border_color);
 }
--- game/menu/include/menu.h
+++ game/menu/include/menu.h
@@ -116,3 +116,77 @@ void GameMenu::draw() {
         m_window.draw(m_menuItemNames[i]);
     }
 }
+
+void InitText(sf::Text& mtext,
+              float xpos,
+              float ypos,
+              sf::String str,
+              int size_font = 60,
+              sf::Color menu_text_color = sf::Color::White,
+              int bord = 0,
+              sf::Color border_color = sf::Color::Black) {
+    mtext.setCharacterSize(size_font);
+    mtext.setPosition(xpos, ypos);
+    mtext.setString(str);
+    mtext.setFillColor(menu_text_color);
+    mtext.setOutlineThickness(bord);
+    mtext.setOutlineColor(border_color);
+}
+
+void start() {
+    sf::RenderWindow window;
+    window.create(sf::VideoMode::getDesktopMode(), L"Моя игра", sf::Style::Fullscreen);
+
+    window.setMouseCursorVisible(false);
+    float width = sf::VideoMode::getDesktopMode().width;
+    float height = sf::VideoMode::getDesktopMode().height;
+
+    sf::RectangleShape background(sf::Vector2f(width, height));
+    sf::Texture texture_window;
+    if (!texture_window.loadFromFile("image/menu_background.jpg")) { exit(5); }
+    background.setTexture(&texture_window);
+
+    sf::Font font;
+    if (!font.loadFromFile("font/MyriadPro.otf")) { exit(5); }
+    sf::Text title;
+    title.setFont(font);
+
+    InitText(title, 480, 50, L"Maddest Pointer", 150, sf::Color(237, 147, 0), 3);
+    sf::String name_menu[]{L"Стрелялка", L"Машинки", L"Выход"};
+
+    GameMenu menu(window, 950, 350, 3, name_menu, 100, 120);
+    menu.setMenuTextColor(sf::Color(237, 147, 0), sf::Color::Red, sf::Color::Black);
+    menu.alignMenuPosition(2);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::KeyReleased) {
+                if (event.key.code == sf::Keyboard::Up) {
+                    menu.moveUp();
+                }
+                if (event.key.code == sf::Keyboard::Down) {
+                    menu.moveDown();
+                }
+                if (event.key.code == sf::Keyboard::Return) {
+                    switch (menu.getNumOfSelectedItem()) {
+                        case 0:
+                            std::cout << "start first game" << std::endl;
+                            break;
+                        case 1:
+                            std::cout << "start second game" << std::endl;
+                            break;
+                        case 2:
+                            window.close();
+                            break;
+                    }
+                }
+            }
+        }
+        window.clear();
+        window.draw(background);
+        window.draw(title);
+        menu.draw();
+        window.display();
+    }
+}
